45fb7487b35c18458d7a017201fb4ddb8735fd8e
Nathalie DE LA BURGADE <nade-la-@e1r9p16.42.fr>
2022-06-24 18:23:21 +0200

    presque presque

---
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -0,0 +1,25 @@
+
+#!/bin/sh
+
+# wait for mysql
+while ! mariadb -h$WORDPRESS_DB_HOST -u$WORDPRESS_DB_USER -p$WORDPRESS_DB_PASSWORD $WORDPRESS_DB_NAME &>/dev/null; do
+    sleep 3
+done
+
+if [ ! -f "/var/www/html/index.php" ]; then
+
+    # static website
+    #mv /tmp/index.html /var/www/html/index.html
+
+    wp core download --allow-root
+    wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
+    wp core install --url=$DOMAIN_NAME/wordpress --title=$WORDPRESS_DB_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email --allow-root
+    wp user create $WORDPRESS_DB_USER  $WORDPRESS_DB_EMAIL --role=author --user_pass=$WORDPRESS_DB_PASSWORD --allow-root
+    wp theme install inspiro --activate --allow-root
+    wp theme update inspiro --allow-root
+    wp plugin update --all --allow-root
+
+fi
+
+echo "Wordpress started on :9000"
+/usr/sbin/php-fpm7 -F -R
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,6 +1,4 @@
-#wget http://wordpress.org/latest.tar.gz &&\
-#    tar -xzvf latest.tar.gz &&\
-#    rm latest.tar.gz && cp -rf /wordpress/* /var/www/html && cp -rf  /wp-config.php /var/www/html
+#!/bin/sh
 
-chown nginx:nginx -R /var/www/html/*
-php-fpm7 -R -F
+wp core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WORDPRESS_DB_TITLE --admin_name=$ORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB__EMAIL --allow-root --skip-email
+php-fpm7 -F -R

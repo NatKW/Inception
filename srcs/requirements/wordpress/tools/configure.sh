056018f4cdd77e7808a1a36a3cbad6ee7cd55dd0
Nathalie DE LA BURGADE <nade-la-@e2r8p9.42.fr>
2022-06-28 19:36:21 +0200

    That's it

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -13,10 +13,10 @@ if [ ! -f "/var/www/html/index.php" ]; then
 
     wp core download --allow-root
     wp config create --dbname=$WORDPRESS_DB_NAME --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --dbhost=$WORDPRESS_DB_HOST --dbcharset="utf8" --dbcollate="utf8_general_ci" --allow-root
-    wp core install --url=$DOMAIN_NAME/wordpress --title=$WORDPRESS_DB_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email --allow-root
-    wp user create $WORDPRESS_DB_USER  $WORDPRESS_DB_EMAIL --role=author --user_pass=$WORDPRESS_DB_PASSWORD --allow-root
-    wp theme install inspiro --activate --allow-root
-    wp theme update inspiro --allow-root
+    wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_DB_TITLE --admin_user=$WORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB_ADMIN_EMAIL --skip-email --allow-root
+    wp user create $WORDPRESS_DB_USER $WORDPRESS_DB_EMAIL --role=author --user_pass=$WORDPRESS_DB_PASSWORD --allow-root
+    wp theme install  twenty sixteen --activate --allow-root
+    wp theme update twenty sixteen --allow-root
     wp plugin update --all --allow-root
 
 fi
--- srcs/requirements/wordpress/tools/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wp core install --path=/var/www/html --url=$DOMAIN_NAME --title=$WORDPRESS_DB_TITLE --admin_name=$ORDPRESS_DB_ADMIN --admin_password=$WORDPRESS_DB_ADMIN_PWD --admin_email=$WORDPRESS_DB__EMAIL --allow-root --skip-email
-php-fpm7 -F -R
